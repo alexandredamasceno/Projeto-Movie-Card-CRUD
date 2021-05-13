9f8997411dcb7065d4b2f63239ad415ae1b6c170
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-05-13 08:43:45 -0300

    Requisito 4

--- src/pages/AddDetails.js
+++ src/pages/AddDetails.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 class AddDetails extends Component {
   render() {
     const { movie } = this.props;
-    const { storyline, imagePath, genre, rating, subtitle } = movie;
+    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <p>{ `Title: ${title}` }</p>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <div>
+          <Link to="/movies/:id/edit">EDITAR</Link>
+          <Link to="/">VOLTAR</Link>
+        </div>
       </div>
     );
   }
@@ -20,6 +26,7 @@ class AddDetails extends Component {
 
 AddDetails.propTypes = {
   movie: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
     subtitle: PropTypes.string,
     storyline: PropTypes.string,
     rating: PropTypes.number,
