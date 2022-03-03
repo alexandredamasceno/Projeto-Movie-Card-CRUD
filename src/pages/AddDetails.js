3777dbb36f4c6aa0baf193cc8a12725679be5484
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2022-03-03 16:55:07 -0300

    Refact: MovieDetails Css

--- src/pages/AddDetails.js
+++ src/pages/AddDetails.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import '../css/MovieDetails.css';
 
 class AddDetails extends Component {
   render() {
@@ -8,17 +9,25 @@ class AddDetails extends Component {
     const { id, title, storyline, imagePath, genre, rating, subtitle } = movie;
 
     return (
-      <div data-testid="movie-details">
-        <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `Title: ${title}` }</p>
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
+      <div data-testid="movie-details" className="container-md">
+        <img className="details-img" alt="Movie Cover" src={ `../${imagePath}` } />
         <div>
-          <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
-          <Link to="/">VOLTAR</Link>
-          <button type="button" onClick={ handleDeleteMovie }>DELETAR</button>
+          <p>{ `Title: ${title}` }</p>
+          <p>{ `Subtitle: ${subtitle}` }</p>
+          <p>{ `Storyline: ${storyline}` }</p>
+          <p>{ `Genre: ${genre}` }</p>
+          <p>{ `Rating: ${rating}` }</p>
+          <div className="btn-group">
+            <Link className="btn btn-success" to="/">VOLTAR</Link>
+            <Link className="btn btn-warning" to={ `/movies/${id}/edit` }>EDITAR</Link>
+            <button
+              className="btn btn-danger"
+              type="button"
+              onClick={ handleDeleteMovie }
+            >
+              DELETAR
+            </button>
+          </div>
         </div>
       </div>
     );
